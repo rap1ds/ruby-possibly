0a8e8523514d61ffdeac2ae90d1601a1d1a85033
Mikko Koski <mikkokos@gmail.com>
2014-04-25 23:56:17 +0300

    .gitignore and LICENSE

--- lib/possibly.rb
+++ lib/possibly.rb
@@ -1,29 +1,45 @@
-def Some(value) Some.new(value); end
-def None() None.new(); end
-def Maybe(value) if value == nil || (value.respond_to?(:length) && value.length == 0) then None() else Some(value) end; end
+module Maybe
+  class Maybe
+    ([:each] + Enumerable.instance_methods).each do |enumerable_method|
+      define_method(enumerable_method) { |*args, &block|
+        res = __enumerable_value.send(enumerable_method, *args, &block)
+        if res.respond_to?(:each) then Maybe(res[0]) else res end
+      }
+    end
+  end
 
-class Maybe; end
-
-class Some < Maybe
-  def get() @value; end
-  def getOrElse(els) @value; end
-  def isSome() true; end
-  def isNone() false; end
-  def initialize(value) @value = value; end
-  def method_missing(method_sym, *args, &block)
-    if [].respond_to? method_sym
-      res = [@value].send(method_sym, *args, &block)
-      if res.respond_to?(:each) then Maybe(res[0]) else res end
-    else
+  class Some < Maybe
+    def get() @value; end
+    def getOrElse(els) @value; end
+    def isSome() true; end
+    def isNone() false; end
+    def initialize(value) @value = value; end
+    def method_missing(method_sym, *args, &block)
       Maybe(@value.send(method_sym, *args, &block))
     end
+
+    private
+    def __enumerable_value() [@value]; end
+  end
+
+  class None < Maybe
+    def get() raise "No such element"; end
+    def getOrElse(els=nil) block_given? ? yield : els; end
+    def isSome() false; end
+    def isNone() true; end
+    def method_missing(method_sym, *args, &block)
+      None.new
+    end
+
+    private
+    def __enumerable_value() []; end
   end
 end
 
-class None < Maybe
-  def get() raise "No such element"; end
-  def getOrElse(els) els; end
-  def isSome() false; end
-  def isNone() true; end
-  def method_missing(method_sym, *args, &block) None(); end
+def Maybe(value)
+  if value == nil || (value.respond_to?(:length) && value.length == 0)
+    Maybe::None.new()
+  else
+    Maybe::Some.new(value)
+  end
 end
--- spec/spec.rb
+++ spec/spec.rb
@@ -1,25 +1,26 @@
-require 'maybe'
+require 'possibly'
 
-describe "maybe" do
+describe "possibly" do
   describe "enumerable" do
     it "#each" do
-      expect { |b| Some(1).each(&b) }.to yield_with_args(1)
-      expect { |b| None().each(&b) }.not_to yield_with_args
+      expect { |b| Maybe::Some.new(1).each(&b) }.to yield_with_args(1)
+      expect { |b| Maybe::None.new.each(&b) }.not_to yield_with_args
     end
 
     it "#map" do
-      expect(Some(2).map { |v| v * v }.get).to eql(4)
-      expect { |b| None().map(&b) }.not_to yield_with_args
+      expect(Maybe::Some.new(2).map { |v| v * v }.get).to eql(4)
+      expect { |b| Maybe::None.new.map(&b) }.not_to yield_with_args
     end
 
     it "#inject" do
-      expect(Some(2).inject(5) { |v| v * v }).to eql(25)
-      expect { |b| None().inject(&b) }.not_to yield_with_args
+      expect(Maybe::Some.new(2).inject(5) { |v| v * v }).to eql(25)
+      expect { |b| Maybe::None.new.inject(&b) }.not_to yield_with_args
+      expect(Maybe::None.new.inject(5) { }).to eql(5)
     end
 
     it "#select" do
-      expect(Some(2).select { |v| v % 2 == 0 }.get).to eql(2)
-      expect(Some(1).select { |v| v % 2 == 0 }.isNone).to eql(true)
+      expect(Maybe::Some.new(2).select { |v| v % 2 == 0 }.get).to eql(2)
+      expect(Maybe::Some.new(1).select { |v| v % 2 == 0 }.isNone).to eql(true)
     end
   end
 
@@ -40,31 +41,32 @@ describe "maybe" do
 
   describe "is_a" do
     it "Some" do
-      expect(Some(1).is_a?(Some)).to eql(true)
-      expect(Some(1).is_a?(None)).to eql(false)
-      expect(None().is_a?(Some)).to eql(false)
-      expect(None().is_a?(None)).to eql(true)
-      expect(Some(1).is_a?(Maybe)).to eql(true)
-      expect(None().is_a?(Maybe)).to eql(true)
+      expect(Maybe::Some.new(1).is_a?(Maybe::Some)).to eql(true)
+      expect(Maybe::Some.new(1).is_a?(Maybe::None)).to eql(false)
+      expect(Maybe::None.new.is_a?(Maybe::Some)).to eql(false)
+      expect(Maybe::None.new.is_a?(Maybe::None)).to eql(true)
+      expect(Maybe::Some.new(1).is_a?(Maybe::Maybe)).to eql(true)
+      expect(Maybe::None.new.is_a?(Maybe::Maybe)).to eql(true)
     end
   end
 
   describe "get and getOrElse" do
     it "get" do
-      expect { None().get }.to raise_error
-      expect(Some(1).get).to eql(1)
+      expect { Maybe::None.new.get }.to raise_error
+      expect(Maybe::Some.new(1).get).to eql(1)
     end
 
     it "getOrElse" do
-      expect(None().getOrElse(true)).to eql(true)
-      expect(Some(1).getOrElse(2)).to eql(1)
+      expect(Maybe::None.new.getOrElse(true)).to eql(true)
+      expect(Maybe::None.new.getOrElse { false }).to eql(false)
+      expect(Maybe::Some.new(1).getOrElse(2)).to eql(1)
     end
   end
 
   describe "forward" do
     it "forwards methods" do
-      expect(Some("maybe").upcase.get).to eql("MAYBE")
-      expect(Some([1, 2, 3]).map { |arr| arr.map { |v| v * v } }.get).to eql([1, 4, 9])
+      expect(Maybe::Some.new("maybe").upcase.get).to eql("MAYBE")
+      expect(Maybe::Some.new([1, 2, 3]).map { |arr| arr.map { |v| v * v } }.get).to eql([1, 4, 9])
     end
   end
 end
