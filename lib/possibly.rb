79335959586eeb7731219644e4b013c1023001c5
Mikko Koski <mikkokos@gmail.com>
2014-05-30 16:49:28 +0300

    Fix style

--- lib/possibly.rb
+++ lib/possibly.rb
@@ -1,10 +1,12 @@
 module Maybe
+  # Parent class for Maybe::Some and Maybe::None. You should never
+  # instantiate this class.
   class Maybe
     ([:each] + Enumerable.instance_methods).each do |enumerable_method|
-      define_method(enumerable_method) { |*args, &block|
+      define_method(enumerable_method) do |*args, &block|
         res = __enumerable_value.send(enumerable_method, *args, &block)
-        if res.respond_to?(:each) then Maybe(res.first) else res end
-      }
+        res.respond_to?(:each) ? Maybe(res.first) : res
+      end
     end
 
     def to_ary
@@ -12,48 +14,90 @@ module Maybe
     end
     alias_method :to_a, :to_ary
 
-    def ==(o)
-      o.class == self.class
+    def ==(other)
+      other.class == self.class
     end
     alias_method :eql?, :==
   end
 
+  # Represents a non-empty value
   class Some < Maybe
-    def get() @value; end
-    def or_else(els=nil) @value; end
-    def is_some?() true; end
-    def is_none?() false; end
-    def initialize(value) @value = value; end
-    def ==(o)
-      super && get == o.get
+    def initialize(value)
+      @value = value
+    end
+
+    def get
+      @value
+    end
+
+    def or_else(*)
+      @value
+    end
+
+    # rubocop:disable PredicateName
+    def is_some?
+      true
+    end
+
+    def is_none?
+      false
+    end
+    # rubocop:enable PredicateName
+
+    def ==(other)
+      super && get == other.get
     end
     alias_method :eql?, :==
+
     def method_missing(method_sym, *args, &block)
       map { |value| value.send(method_sym, *args, &block) }
     end
 
     private
-    def __enumerable_value() [@value]; end
+
+    def __enumerable_value
+      [@value]
+    end
   end
 
+  # Represents an empty value
   class None < Maybe
-    def get() raise "No such element"; end
-    def or_else(els=nil) block_given? ? yield : els; end
-    def is_some?() false; end
-    def is_none?() true; end
-    def method_missing(method_sym, *args, &block)
+    def get
+      fail 'No such element'
+    end
+
+    def or_else(els = nil)
+      block_given? ? yield : els
+    end
+
+    # rubocop:disable PredicateName
+    def is_some?
+      false
+    end
+
+    def is_none?
+      true
+    end
+    # rubocop:enable PredicateName
+
+    def method_missing(*)
       None.new
     end
 
     private
-    def __enumerable_value() []; end
+
+    def __enumerable_value
+      []
+    end
   end
 end
 
+# rubocop:disable MethodName
 def Maybe(value)
-  if value == nil || (value.respond_to?(:length) && value.length == 0)
-    Maybe::None.new()
+  if value.nil? || (value.respond_to?(:length) && value.length == 0)
+    Maybe::None.new
   else
     Maybe::Some.new(value)
   end
 end
+# rubocop:enable MethodName
